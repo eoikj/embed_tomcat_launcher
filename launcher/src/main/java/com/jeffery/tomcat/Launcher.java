b586cfa242f00388c4da152e78248e2bdf7afeb4
Eternity <xeternity@qq.com>
2020-05-04 22:32:57 +0800

    init

---
+++ launcher/src/main/java/com/jeffery/tomcat/Launcher.java
@@ -0,0 +1,73 @@
+package com.jeffery.tomcat;
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.jmx.ManagementContext;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.AprLifecycleListener;
+import org.apache.catalina.deploy.NamingResourcesImpl;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
+
+public class Launcher {
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.addWebapp("/test","D:/code/tomcatsuit/war/target/war-0.0.1-SNAPSHOT");
+        tomcat.setPort(8080);
+        AprLifecycleListener lifecycleListener = new AprLifecycleListener();
+        tomcat.getServer().addLifecycleListener(lifecycleListener);
+
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        tomcat.getService().addConnector(connector);
+
+        NamingResourcesImpl globalNamingResources = tomcat.getServer().getGlobalNamingResources();
+        globalNamingResources.addResource(jndiDb());
+        globalNamingResources.addResource(jmsConnFactory(10081,"sample","brkSample"));
+        globalNamingResources.addResource(jmsQueue("sample","P.Sample"));
+        tomcat.enableNaming();
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    private static ContextResource jndiDb() {
+        ContextResource jndi = new ContextResource();
+        jndi.setName("jdbc/db");
+        jndi.setAuth("Container");
+        jndi.setType("javax.sql.DataSource");
+        jndi.setScope("Sharable");
+        jndi.setProperty("driverClassName", "com.mysql.jdbc.Driver");
+        jndi.setProperty("url", "jdbc:mysql://localhost:3306");
+        return jndi;
+    }
+
+    public static ContextResource jmsConnFactory(final int port, final String name, final String brokerName){
+        ContextResource contextResource = new ContextResource();
+        contextResource.setName("jms/"+name);
+        contextResource.setAuth("Container");
+        contextResource.setType("org.apache.activemq.ActiveMQConnectionFactory");
+        contextResource.setProperty("factory","org.apache.activemq.jndi.JNDIReferenceFactory");
+        contextResource.setProperty("brokerURL","tcp://localhost:"+port);
+        contextResource.setProperty("brokerName",brokerName);
+        contextResource.setProperty("useEmbeddedBroker","true");
+        return contextResource;
+    }
+
+    public static ContextResource jmsQueue(final String name, final String physicalName){
+        ContextResource contextResource = new ContextResource();
+        contextResource.setName("jms/queue/"+name);
+        contextResource.setAuth("Container");
+        contextResource.setType("org.apache.activemq.command.ActiveMQQueue");
+        contextResource.setProperty("factory","org.apache.activemq.jndi.JNDIReferenceFactory");
+        contextResource.setProperty("physicalName",physicalName);
+        return contextResource;
+    }
+
+    public static BrokerService mq(int port,String embedMQ) throws Exception {
+        BrokerService brokerService = new BrokerService();
+        brokerService.setBrokerName(embedMQ);
+        brokerService.addConnector("tcp://localhost:"+port);
+        brokerService.setManagementContext(new ManagementContext());
+        return brokerService;
+    }
+}
---
+++ war/src/main/java/com/jeffery/war/SpringbootLauncher.java
@@ -0,0 +1,21 @@
+package com.jeffery.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootLauncher extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLauncher.class, args);
+    }
+
+    @RequestMapping("/*")
+    public String sample(){
+        return "it works!";
+    }
+}
